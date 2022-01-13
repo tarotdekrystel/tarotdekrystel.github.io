f2eed30f69fe602da4fe5605af8cca4b5cd116e5
tarotdekrystel <96932360+tarotdekrystel@users.noreply.github.com>
2022-01-13 06:07:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+function menu() {
+  var menuE = document.getElementById("enlaces").attributes.item("class").value;
+  if (menuE == "menuC") {
+    document.getElementById("enlaces").attributes.item("display").value = "menuA";
+  } else if (menuE == "menuA") {
+    document.getElementById("enlaces").attributes.item("display").value = "menuC";
+  }
+  return 0;
+}
