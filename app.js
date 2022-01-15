1456c4dca7c2378987b552023800d15d6acee4b7
tarotdekrystel <96932360+tarotdekrystel@users.noreply.github.com>
2022-01-15 04:49:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function cookieEditar(cNombre, cValor, cCaduca, cRuta) {
+  if (cNombre == "") {
+    console.log("Nombre de cookie no definido.");
+  } else if (cValor == "") {
+    console.log("Valor de cookie no definido.");
+  } else if (cCaduca == "" && cRuta == "") {
+    document.cookie = '"' + cNombre + '=' + cValor + '"';
+  } else if (cCaduca == "" && cRuta != "") {
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + '' + '; path=/' + cRuta + '"';
+  } else if (cCaduca != "" && cRuta == "") {
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '"';
+  } else {
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '; path=/' + cRuta + '"';
+  }
+  return 0;
+}
+
+function formulario() {
+    location.assign("./form.html");
+    return 0;
+}
+
 function menu() {
   var menuE = document.getElementById("enlaces").attributes.item("class").value;
   if (menuE == "menuC") {
@@ -6,9 +28,4 @@ function menu() {
     document.getElementById("enlaces").attributes.item("display").value = "menuC";
   }
   return 0;
-}
-
-function formulario() {
-    location.assign("./form.html");
-    return 0;
 }
