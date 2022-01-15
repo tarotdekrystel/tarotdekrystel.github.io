dc31ec92d297ee4ba611ddfdeb20ab5c618b7868
tarotdekrystel <96932360+tarotdekrystel@users.noreply.github.com>
2022-01-15 04:54:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function cargarFormulario() {
+  /* Comprobar si el navegador acepta cookies; en caso contrario, enviar un mensaje de notificación y opción de activarlas. */
+  cookieComprobarHabilitado();
+  /* Iniciar cookie, para guardar la semana mostrada. */
+  cookieCrear("semana", "0", "", "");
+  /* Calcular la diferencia horaria, respecto al usuario. */
+  calcularDiferencia();
+  /* Ajustar los horarios de atención, a la franja horaria del usuario. */
+  ajustarHoras();
+  /* Ajustar los días de la semana inicial, dependiendo de la fecha actual. */
+  ajustarSemanaIni();
+  /* Ajustar las casillas de selección, de la semana inicial, en función de la fecha actual. */
+  ajustarCasillas();
+  return 0;
+}
+
+function cookieComprobarHabilitado() {
+  if (cHabil == true) {
+    return 0;
+  } else {
+    alert('Tiene deshabilitado el uso de "cookies"; las cuales necesitamos, para el correcto funcionamiento, de algunas funciones de esta página. Sin acceso al uso de "cookies", solo podemos mostrarle, la semana actual; excepto si hoy es domingo, en cuyo caso verá, la semana siguiente.');
+  }
+  return 0;
+}
+
 function cookieEditar(cNombre, cValor, cCaduca, cRuta) {
   if (cNombre == "") {
     console.log("Nombre de cookie no definido.");
@@ -15,7 +40,7 @@ function cookieEditar(cNombre, cValor, cCaduca, cRuta) {
   return 0;
 }
 
-function formulario() {
+function irAFormulario() {
     location.assign("./form.html");
     return 0;
 }
