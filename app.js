c480f5ff73e3ad3554ca40d2564f3f031ff39380
tarotdekrystel <96932360+tarotdekrystel@users.noreply.github.com>
2022-01-19 16:39:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-/* ÍNDICE:
-   =======
-- Variables de tarotistas.
-- Variables de fecha y hora.
-- Variables de calendario.
-- Variables de hoja de cálculo.
-- Variables de cookies y almacenamiento.
-- ajustarHoras(): Función para ajustar los intervalos del formulario, a la franja horaria del usuario.
-- ajustarSemanaIni(): Función para ajustar, la ubicación del día de hoy, dentro de la semana inicial; y en caso de ser domingo, mostrar la semana siguiente.
-- calcularDiferencia(): Función para calcular la diferencia horaria, entre el servidor y el usuario.
-- cargarFormulario(): Función inicial, tras cargar la página de formulario, de comprobaciones y ajustes iniciales.
-- cookieComprobarHabilitado(): Comprobar si están habilitadas las cookies, en el navegador del usuario; y en caso contrario, mandar un aviso.
-- cookieEditar(): Crear y editar cookies.
-- cookieLeer(): Función para comprobar, si existe una cookie; y en caso afirmativo, leer su valor.
-- irAFormulario(): Función del botón "Reservar Cita", para ir a la página de formulario.
-- menu(): Función para abrir y cerrar el menú de navegación, en la versión móvil.
-*/
-
-// Variables de tarotistas:
 var tarotista = "Krystel";
-
-// Variables de fecha y hora:
 var fechaUsuarioHoy = new Date();
 var fechaTarotHoy = new Date ();
 var lunes = new Date();
@@ -35,27 +14,29 @@ var hoyAnio;
 var hoyMes;
 var hoyDiaMes;
 var hoySemana;
-
-// Variables de calendario:
 var calendario;
 var eventos;
-
-// Variables de hoja de cálculo:
 var libro;
 var hoja;
 var rango;
-
-// Variables de cookies y almacenamiento:
 var cHabil = navigator.cookieEnabled;
 var cookies = document.cookie;
 var cookieSemana;
+var cookieServicio;
 
-// Función para ajustar en el formulario, las casillas de selección, en función de la semana a visualizar y la fecha actual:
-function ajustarCasillas() {
+function ajustarDiferenciaHoraria() {
+  fechaTarotHoy.setFullYear(fechaUsuarioHoy.getFullYear(), fechaUsuarioHoy.getMonth(), fechaUsuarioHoy.getDate());
+  fechaTarotHoy.setHours(fechaUsuarioHoy.getHours() + diferenciaHoras);
+  fechaTarotHoy.setMinutes(fechaUsuarioHoy.getMinutes());
+  fechaTarotHoy.setSeconds(fechaUsuarioHoy.getSeconds());
+  fechaTarotHoy.setMilliseconds(fechaUsuarioHoy.getMilliseconds());
+  hoyAnio = fechaTarotHoy.getFullYear();
+  hoyMes = fechaTarotHoy.getMonth();
+  hoyDiaMes = fechaTarotHoy.getDate();
+  hoySemana = fechaTarotHoy.getDay();
 }
 
-// Función para ajustar los intervalos del formulario, a la franja horaria del usuario:
-function ajustarHoras() {
+function ajustarHorasIntervalos() {
   var minIni = "";
   var minFin = "";
   var hI;
@@ -66,52 +47,63 @@ function ajustarHoras() {
     if (r == 0) {
       minIni = "45";
       minFin = "00";
-    } else if (r == 1) {
+    }
+    else if (r == 1) {
       minIni = "00";
       minFin = "15";
-    } else if (r == 2) {
+    }
+    else if (r == 2) {
       minIni = "15";
       minFin = "30";
-    } else if (r == 3) {
+    }
+    else if (r == 3) {
       minIni = "30";
       minFin = "45";
-    } else {
+    }
+    else {
       console.log("Error en el cálculo de minutos del formulario.");
     }
     if (d < 1) {
       hI = hInicio;
       hF = hInicio;
-    } else if (d == 1) {
+    }
+    else if (d == 1) {
       hI = hInicio;
       hF = hInicio + 1;
-    } else if (1 < d && d < 2) {
+    }
+    else if (1 < d && d < 2) {
       hI = hInicio + 1;
       hF = hInicio + 1;
-    } else if (d == 2) {
+    }
+    else if (d == 2) {
       hI = hInicio + 1;
       hF = hInicio + 2;
-    } else if (2 < d && d < 3) {
+    }
+    else if (2 < d && d < 3) {
       hI = hInicio + 2;
       hF = hInicio + 2;
-    } else if (d == 3) {
+    }
+    else if (d == 3) {
       hI = hInicio + 2;
       hF = hInicio + 3;
-    } else if (3 < d && d < 4) {
+    }
+    else if (3 < d && d < 4) {
       hI = hInicio + 3;
       hF = hInicio + 3;
-    } else if (d == 4) {
+    }
+    else if (d == 4) {
       hI = hInicio + 3;
       hF = hInicio + 4;
-    } else {
+    }
+    else {
       console.log("Error en el cálculo de horas del formulario.");
     }
-    var rango = "hora" + i;
-    document.getElementsyClassName(rango).outerText(hI + ":" + minIni + "-" + hF + ":" + minFin);
+    rango = "hora-" + i;
+    document.getElementById(rango).innerHTML = hI + ":" + minIni + " - " + hF + ":" + minFin;
   }
 }
 
-// Función para ajustar, la ubicación del día de "hoy", dentro de la semana inicial; y en caso de ser domingo, mostrar la semana siguiente:
-function ajustarSemanaIni() {
+function ajustarSemanaInicial() {
   if (hoySemana == 0) {
     lunes = new Date(fechaTarotHoy.getFullYear(), fechaTarotHoy.getMonth(), fechaTarotHoy.getDate() + 1);
     domingo = new Date(fechaTarotHoy.getFullYear(), fechaTarotHoy.getMonth(), fechaTarotHoy.getDate() + 7);
@@ -136,110 +128,83 @@ function ajustarSemanaIni() {
   } else {
     console.log('Error en el ajuste de dias, al cargar la semana actual.');
   }
+  document.getElementById("semana").innerHTML = (lunes.getDate() + "/" + lunes.getMonth() + 1 + "/" + lunes.getFullYear() + " - " + domingo.getDate() + "/" + domingo.getMonth() + 1 + "/" + domingo.getFullYear());
 }
 
-// Función para calcular la diferencia horaria, entre el servidor y el usuario:
-function calcularDiferencia() {
-  fechaTarotHoy.setFullYear(fechaUsuarioHoy.getFullYear(), fechaUsuarioHoy.getMonth(), fechaUsuarioHoy.getDate());
-  fechaTarotHoy.setHours(fechaUsuarioHoy.getHours() + diferenciaHoras);
-  fechaTarotHoy.setMinutes(fechaUsuarioHoy.getMinutes());
-  fechaTarotHoy.setSeconds(fechaUsuarioHoy.getSeconds());
-  fechaTarotHoy.setMilliseconds(fechaUsuarioHoy.getMilliseconds());
-  hoyAnio = fechaTarotHoy.getFullYear();
-  hoyMes = fechaTarotHoy.getMonth();
-  hoyDiaMes = fechaTarotHoy.getDate();
-  hoySemana = fechaTarotHoy.getDay();
-}
-
-// Función para leer en el calendario, los intervalos disponibles y ocupados, de la semana a visualizar.
-function calendarioLeerDisponible() {
-}
-
-// Función inicial, al cargar la página de formulario:
 function cargarFormulario() {
-  if (cHabil == true) { // En caso de estar habilitado el uso de cookies, iniciar una, para registrar la semana que se visualiza.
-    var servicio = cookieLeer("servicio");
-    console.log(servicio);
-    if (servicio == "suenos") { // Si se ha guardado la cookie correspondiente, marcar la opción asociada al enlace, desde el que se llamó al formulario.
-      document.getElementById("Sueno").setAttribute("checked");
-    } else if (servicio == "tarot") {
-      document.getElementById("Tarot").setAttribute("checked");
-    }
-    cookieEditar("semana", "0", "2", "");
-  }
-  calcularDiferencia(); // Calcular la diferencia horaria, respecto al usuario.
-  ajustarHoras(); // Ajustar los horarios de atención, a la franja horaria del usuario.
-  ajustarSemanaIni(); // Ajustar los días de la semana inicial, dependiendo de la fecha actual.
-  calendarioLeerDisponible(); // Lleer en el calendario, los intervalos disponibles y ocupados, de la semana a visualizar; si es la actual, a partir de "mañana".
-  ajustarCasillas(); // Ajustar en el formulario, las casillas de selección de la semana inicial, en función de la fecha actual.
+  cookieEditar("semana", semana, "", "");
+  ajustarDiferenciaHoraria();
+  ajustarHorasIntervalos();
+  ajustarSemanaInicial();
+  ajustarCasillasCitas();
   return 0;
 }
 
-// Función para comprobar si están habilitadas las cookies, en el navegador del usuario:
+function cookieComprobarExiste() {
+}
+
 function cookieComprobarHabilitado() {
   if (cHabil == true) {
-    return 0;
-  } else {
+  } else if (cHabil == false) {
     alert('Tiene deshabilitado el uso de "cookies"; las cuales necesitamos, para el correcto funcionamiento, de algunas funciones de esta página. Sin acceso al uso de "cookies", solo podemos mostrarle, la semana actual; excepto si hoy es domingo, en cuyo caso verá, la semana siguiente.');
+  } else {
   }
-  return 0;
 }
 
-// Función para crear y editar cookies:
 function cookieEditar(cNombre, cValor, cCaduca, cRuta) {
-  if (cNombre == "" || cNombre == "null") {
+  if (cNombre == "") {
     console.log("Nombre de cookie no definido.");
-  } else if (cValor == "" || cValor == "null") {
+  } else if (cValor == "") {
     console.log("Valor de cookie no definido.");
   } else if (cCaduca == "" && cRuta == "") {
-    galleta = '"' + cNombre + '=' + cValor + '"';
-    document.cookie = encodeURIComponent(galleta);
+    document.cookie = '"' + cNombre + '=' + cValor + '"';
   } else if (cCaduca == "" && cRuta != "") {
-    galleta = '"' + cNombre + '=' + cValor + '; expires=' + '' + '; path=/' + cRuta + '"';
-    document.cookie = encodeURIComponent(galleta);
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + '' + '; path=/' + cRuta + '"';
+  } else if (cCaduca != "" && cRuta == "") {
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '"';
   } else {
-    var caduca = new Date();
-    caduca.setTime(getTime() + (cCaduca * 60 * 60 * 1000));
-    cCaduca = caduca.toUTCString();
-    if (cCaduca != "" && cRuta == "") {
-      galleta = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '"';
-      document.cookie = encodeURIComponent(galleta);
-    } else {
-      galleta = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '; path=/' + cRuta + '"';
-      document.cookie = encodeURIComponent(galleta);
-    }
+    document.cookie = '"' + cNombre + '=' + cValor + '; expires=' + cCaduca + '; path=/' + cRuta + '"';
   }
+}
+
+function cookieEliminar() {
   return 0;
 }
 
-// Función para comprobar, si existe una cookie; y en caso afirmativo, leer su valor:
-function cookieLeer(nombreCookie) {
-  var cookies = decodeURIComponent(document.cookie).split(';');
-  for (var i = 0; i < cookies.length; i++) {
-    var ck = cookies[i];
-    while (ck.charAt(0) == ' ') {
-      ck = ck.substring(1);
-    }
-    if (ck.indexOf(nombreCookie + '=') == 0) {
-      return ck.substring(nombreCookie.length, ck.length);
-    }
-  }
-  return "";
-}
-
-// Función del botón "Reservar Cita", para ir al formulario.
-function irAFormulario(servicio) {
-  location.assign("./form.html");
+function cookieLeer() {
   return 0;
 }
 
-// Función para abrir y cerrar el menú de navegación, en la versión móvil:
+function enviarForm() {
+  return 0;
+}
+
+function irAFormulario() {
+    location.assign("./form.html");
+    return 0;
+}
+
 function menu() {
   var menuE = document.getElementById("enlaces").attributes.item("class").value;
   if (menuE == "menuC") {
     document.getElementById("enlaces").attributes.item("display").value = "menuA";
-  } else if (menuE == "menuA") {
+  } else {
     document.getElementById("enlaces").attributes.item("display").value = "menuC";
   }
+  return 0;
+}
+
+function quitarHoyYAnteriores() {
+}
+
+function semanaActual() {
+  return 0;
+}
+
+function semanaAnterior() {
+  return 0;
+}
+
+function semanaSiguiente() {
   return 0;
 }
